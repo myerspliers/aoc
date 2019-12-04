40748ec63c255f86d600f5472c8b15a6480b05d1
Matthew Myers <matthew_myers@bose.com>
2019-12-04 10:35:23 -0700

    Replace day 1 tests that got lost

--- aoc2019/test/day1_test.exs
+++ aoc2019/test/day1_test.exs
@@ -2,7 +2,16 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "greets the world" do
-    assert Aoc2019.hello() == :world
+  test "Part 1 calc_fuel_required1" do
+    assert Day1.calc_fuel_required1("12") == 2
+    assert Day1.calc_fuel_required1("14") == 2
+    assert Day1.calc_fuel_required1("1969") == 654
+    assert Day1.calc_fuel_required1("100756") == 33583
+  end
+
+  test "Part 2 complete fuel requirements (good) fuel" do
+    assert Day1.calc_fuel_required2("14") == 2
+    assert Day1.calc_fuel_required2("1969") == 966
+    assert Day1.calc_fuel_required2("100756") == 50346
   end
 end
