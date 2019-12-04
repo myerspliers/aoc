ad38e4c105b482396cc2235972dbef735b1d27a6
Matthew Myers <matt.myers.real@gmail.com>
2019-12-04 11:27:27 -0700

    More day 2 boilerplate

---
+++ aoc2019/lib/day2.ex
@@ -0,0 +1,6 @@
+defmodule Day2 do
+  @moduledoc """
+  Solutions for day 2
+  """
+
+end
---
+++ aoc2019/test/day2_test.exs
@@ -0,0 +1,12 @@
+defmodule Day2Test do
+  use ExUnit.Case
+  doctest Day2
+
+  test "Part 1 " do
+    #assert Day2.x() == 0
+  end
+
+  test "Part 2" do
+    #assert Day2.y() == 0
+  end
+end
